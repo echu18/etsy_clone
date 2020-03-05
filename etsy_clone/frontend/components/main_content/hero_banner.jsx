46417acef5265b1710d437f468b6698a2e34354c
echu18 <elizabethchu18@gmail.com>
2020-03-05 09:53:50 -0800

    Update hero banner with images

--- etsy_clone/frontend/components/main_content/hero_banner.jsx
+++ etsy_clone/frontend/components/main_content/hero_banner.jsx
@@ -3,24 +3,33 @@ import { Link } from 'react-router-dom';
 
 const HeroBanner = (props) => (
     <div className='main-hero-banner' id='inner'>
-        <div className='hero-block hero-large'>
-            <div className='text-container'>
-                <p>Standing out just got so easy.</p>
-                <p>Shop fashion > </p>
-                <img className='hero-large-img' src="" alt=""/>
+        <div className='hero-block-large'>
+                <div className='hero-large-text'>
+                    <h3>Standing out just got so easy.</h3>
+                    <p>Shop fashion > </p>
+                </div>
+                
+            <div className='hero-container'>
+                <img className='hero-large-img' src='https://etsy-clone-dev.s3-us-west-1.amazonaws.com/splash-images/hero-img-large-resize.jpg'/>
             </div>
         </div>
 
         <div className='hero-medium-small'>
-            <div className='hero-block hero-medium'>
-                <p>Bridesmaids must-haves</p>
-                <p>Shop gifts > </p>
-                <img className='hero-medium-img' src="" alt="" />
+            <div className='hero-block-medium'>
+                    <div className='hero-medium-text'>
+                        <h3>Bridesmaids must-haves</h3>
+                        <p>Shop gifts > </p>
+                    </div>
+                <div className='hero-container'> { /* hero container div maintains margin and whitespace */}
+                    <img className='hero-medium-img' src="https://etsy-clone-dev.s3-us-west-1.amazonaws.com/splash-images/hero-medium.jpg"/>
+                </div>
             </div>
 
-            <div className='hero-block hero-small'>
-                <div className='text-container'>
-                    <p>Free shipping coming right up > </p>
+            <div className='hero-block-small'>
+                <div className='hero-container'>
+                    <div className='hero-small-text'>
+                        <p>Free shipping coming right up > </p>
+                    </div>
                 </div>
             </div>
         </div>
