0c3024edf2376b179b4222c7bc07a08fdcd492e4
echu18 <elizabethchu18@gmail.com>
2020-03-04 21:35:25 -0800

    heroku push

--- etsy_clone/config/application.rb
+++ etsy_clone/config/application.rb
@@ -10,7 +10,7 @@ module EtsyClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
 
 
     # Settings in config/environments/* take precedence over those specified here.
