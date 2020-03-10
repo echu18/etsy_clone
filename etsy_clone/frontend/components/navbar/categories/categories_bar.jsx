c4bc5fd8ccb08923a3c0c9c70ee284eff61bdd4d
echu18 <elizabethchu18@gmail.com>
2020-03-10 15:35:40 -0700

    Fix navbar resizing

--- etsy_clone/frontend/components/navbar/categories/categories_bar.jsx
+++ etsy_clone/frontend/components/navbar/categories/categories_bar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { giftIcon } from '../../../../app/assets/images/svgs/icons';
+
 
 
 // Links to each category
-const categories = ['Jewelry & Accessories', 'Clothing & Shoes', 'Home & Living', 'Wedding & Party', 'Toys & Entertainment', 'Art & Collectibles', 'Craft Supplies', 'Vintage', 'Gifts'];
+const categories = ['Jewelry & Accessories', 'Clothing & Shoes', 'Home & Living', 'Wedding & Party', 'Toys & Entertainment', 'Art & Collectibles', 'Craft Supplies', 'Vintage'];
 
 const categoriesBar = props => (
     <ul>
@@ -13,6 +15,11 @@ const categoriesBar = props => (
                         <categoryDropdownContainer header={cat} />
                     </li>
         })}
+        <li className='cat-single'>
+            <h2 className='cat-header'><div className='gift-icon'>{giftIcon}</div>Gifts</h2>
+            <categoryDropdownContainer header={'Gifts'} />
+        </li>
+
     </ul>
 )
 
